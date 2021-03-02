aeca3b3a71420fa6e03622a61617f4199ee10c4b
Pulkit Jasti <pulkit.jasti@gmail.com>
2021-03-02 20:43:39 +0530

    Contact section completed

--- script.js
+++ script.js
@@ -10,34 +10,3 @@ cursor.over('.line-1', {
 	borderColor: 'rgba(255,255,255,.38)',
 	broderWidth: 2,
 });
-
-//
-//
-//
-// Client carousel
-
-$(document).ready(function () {
-	$('.customer-logos').slick({
-		slidesToShow: 4,
-		slidesToScroll: 1,
-		autoplay: true,
-		autoplaySpeed: 1000,
-		arrows: false,
-		dots: false,
-		pauseOnHover: false,
-		responsive: [
-			{
-				breakpoint: 768,
-				settings: {
-					slidesToShow: 3,
-				},
-			},
-			{
-				breakpoint: 520,
-				settings: {
-					slidesToShow: 2,
-				},
-			},
-		],
-	});
-});
